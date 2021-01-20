8caad48630e53ca4fe3e7f5ed40490e2acd6ef24
Joep Hinderink <hello@joephinderink.nl>
2021-01-20 22:00:07 +0100

    Initial Commit

---
+++ SearchBar/SearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarApp.swift
+//  SearchBar
+//
+//  Created by Joep Hinderink on 20/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
